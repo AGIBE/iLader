afed99963aa8ac2621d870032f943280bfdf9b79
Peter Schär <peter.schaer@bve.be.ch>
2015-01-14 11:27:07 +0100

    Funktion Begleitdaten hinzugefügt

---
+++ iLader/functions/Begleitdaten.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 14.01.2015
+
+@author: Peter Schär
+'''
+from .TemplateFunction import TemplateFunction
+
+class Begleitdaten(TemplateFunction):
+    '''
+    Führt die Qualitätschecks mit dem QA-Framework aus
+    '''
+
+    def __init__(self, logger, task_config):
+        '''
+        Constructor
+        :param logger: vom Usecase initialisierter logger (logging.logger)
+        :param task_config: Vom Usecase initialisierte task_config (Dictionary)
+        '''
+        self.name = u"Begleitdaten"
+        TemplateFunction.__init__(self, logger, task_config)
+        
+        if self.name in self.task_config['ausgefuehrte_funktionen'] and self.task_config['task_config_load_from_JSON']:
+            self.logger.info(u"Funktion " + self.name + u" wird ausgelassen.")
+        else:
+            self.logger.info(u"Funktion " + self.name + u" wird ausgeführt.")
+            self.__execute()
+        
+
+    def __execute(self):
+        '''
+        Führt den eigentlichen Funktionsablauf aus
+        '''
+        
+        self.logger.info(u"Die Funktion " + self.name + u" arbeitet vor sich hin")
+        
+       
+        self.finish()
