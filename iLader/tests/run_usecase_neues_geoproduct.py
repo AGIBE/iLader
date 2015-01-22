e7e88e1c06b9aa96fdd254cf425c0d76f183b936
Peter Schär <peter.schaer@bve.be.ch>
2015-01-22 14:30:42 +0100

    __future__-Import vergessen

--- iLader/tests/run_usecase_neues_geoproduct.py
+++ iLader/tests/run_usecase_neues_geoproduct.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
 from iLader.usecases import NeuesGeoprodukt
 
-uc = NeuesGeoprodukt("23424234", True)
-print uc.name
-print uc.task_id
+uc = NeuesGeoprodukt("666", False)
+print(uc.name)
+print(uc.task_id)
+print(uc.general_config)
