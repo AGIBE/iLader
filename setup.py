45c85c9bfa17c909ea212d463989f7482fb3ac1b
Peter Schär <peter.schaer@bve.be.ch>
2015-01-29 13:47:20 +0100

    Mail und URL ergänzt.

--- setup.py
+++ setup.py
@@ -12,5 +12,7 @@ setup(
       install_requires = ["configobj==5.0.6", "cx-Oracle==5.1.3", "numpy==1.7.1", "six==1.9.0", "pyasn1==0.1.7", "pycrypto==2.6.1", "python-keyczar==0.715"],
       # PyPI metadata
       author = "Peter Schär, Manuela Uhlmann",
-      description = "Import-Modul Geodatenbank des Kantons Bern"
+      author_email = "peter.schaer@bve.be.ch, manuela.uhlmann@bve.be.ch",
+      description = "Import-Modul Geodatenbank des Kantons Bern",
+      url = "http://www.be.ch/geoportal"
 )
