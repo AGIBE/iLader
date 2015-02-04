7ba756160812cf0fbed7dc2c79575595bb47094a
Manuela Uhlmann <manuela.uhlmann@bve.be.ch>
2015-02-04 13:06:03 +0100

    Funktion IndicesVek2

--- iLader/functions/IndicesVek2.py
+++ iLader/functions/IndicesVek2.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 from .TemplateFunction import TemplateFunction
+import md5
+import arcpy
 
 class IndicesVek2(TemplateFunction):
     '''
@@ -33,10 +35,29 @@ class IndicesVek2(TemplateFunction):
 
     def __execute(self):
         '''
-        Führt den eigentlichen Funktionsablauf aus
+        Führt den eigentlichen Funktionsablauf aus.
+        Iteriert durch alle Vektorebenen (inkl. Wertetabellen) und erstellt die dort
+        aufgeführten Indizes für die Instanz VEK2.
         '''
-        
-        self.logger.info(u"Die Funktion " + self.name + u" arbeitet vor sich hin")
-        
+        for ebene in self.task_config['vektor_ebenen']:
+                if len(ebene["indices"]) > 0:
+                    target = ebene['ziel_vek2']
+                    ebename = ebene['gpr_ebe']
+                    self.logger.info(u"Erstelle Index für " + ebename + u" im VEK2.")           
+                    for index in ebene["indices"]:
+                        try:
+                            self.logger.info("Index: ") 
+                            index_attribute = index['attribute'].replace(", ", ";")
+                            hashfunc = md5.new(ebename.upper() + "." + index_attribute.upper())
+                            indexname = "GEODB_" + hashfunc.hexdigest()[0:10]
+                            if index['unique'] == "False":
+                                indextyp = 'NON_UNIQUE'
+                            elif index['unique'] == "True":
+                                indextyp = 'UNIQUE'
+                            arcpy.AddIndex_management(target, index_attribute, indexname, indextyp, "")
+                            self.logger.info(index_attribute + ": " + indextyp)
+                        except Exception as e:
+                            self.logger.info(u"Fehler bei der Erstellung des Index " + index_attribute + ", " + indextyp)
+                            self.logger.info(e)
        
         self.finish()
