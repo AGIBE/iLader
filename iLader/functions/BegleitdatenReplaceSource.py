cba6a9c18e1519cb5d06208ce64b3750c90e969a
Manuela Uhlmann <manuela.uhlmann@bve.be.ch>
2015-06-02 18:54:57 +0200

    Erweitung BegleitdatenReplaceSource

--- iLader/functions/BegleitdatenReplaceSource.py
+++ iLader/functions/BegleitdatenReplaceSource.py
@@ -28,6 +28,21 @@ class BegleitdatenReplaceSource(TemplateFunction):
             self.start()
             self.__execute()
             
+    def __replace(self, legende, sde_vektoren, sde_raster):
+        self.legende = legende
+        self.sde_vektoren = sde_vektoren
+        self.sde_raster = sde_raster
+        lyr = arcpy.mapping.Layer(self.legende)
+        if lyr.supports("DATASOURCE"):
+            desc = arcpy.Describe(lyr.dataSource)
+            datentyp = desc.dataType
+            if datentyp == "FeatureLayer": #genaue Ausgabewerte noch prüfen
+                self.logger.info("Datentyp: " + datentyp)
+                #TODO: lyr.replaceDataSource(sde_vektoren, "SDE_WORKSPACE", "", "false")
+            elif datentyp == "RasterLayer": # genaue Ausgabewerte noch prüfen
+                self.logger.info("Datentyp: " + datentyp)
+                #TODO: lyr.replaceDataSource(sde_raster, "SDE_WORKSPACE", "", "false")
+        lyr.save()
     def __execute(self):
         '''
         Führt den eigentlichen Funktionsablauf aus.
@@ -35,31 +50,32 @@ class BegleitdatenReplaceSource(TemplateFunction):
         müssen auf ras1 zeigen.
         Vektoren zeigen auf vek1 bzw. vek3
         '''
+              
+        self.sde_conn_vek1_geo = self.task_config['connections']['sde_conn_vek1_geo']
+        self.sde_conn_vek3_geo = self.task_config['connections']['sde_conn_vek3_geo']
+        self.sde_conn_ras1_geo = self.task_config['connections']['sde_conn_ras1_geo']
         # Legenden
-        self.logger.info("Legendenfiles bearbeitet")
+        self.logger.info("Legendenfiles bearbeiten")
         for legende in self.task_config["legende"]:
             self.logger.info("Legende " + legende["ziel_akt"] + " wird bearbeitet.")
-            lyr = arcpy.mapping.Layer(legende["ziel_akt"])
-            desc = arcpy.Describe(lyr)
-            datentyp = desc.dataType
-            if datentyp == "FeatureLayer":
-                self.logger.info("Datentyp: " + datentyp)
-                #TODO: lyr.replaceDataSource(geo_vek1, "SDE_WORKSPACE", "", "false")
+            self.__replace(self, legende["ziel_akt"], self.sde_conn_vek1_geo, self.sde_conn_ras1_geo)
             self.logger.info("Legende " + legende["ziel_zs"] + " wird bearbeitet.")
-            lyr = arcpy.mapping.Layer(legende["ziel_zs"])
-            desc = arcpy.Describe(lyr)
-            datentyp = desc.dataType
-            if datentyp == "FeatureLayer":
-                self.logger.info("Datentyp: " + datentyp)
-                #TODO: lyr.replaceDataSource(workspace_path, workspace_type, dataset_name, validate)
-        
+            self.__replace(self, legende["ziel_zs"], self.sde_conn_vek3_geo, self.sde_conn_ras1_geo)
+                    
         # MXDs
-        self.logger.info("MXD-Files auslesen")
+        self.logger.info("MXD-Files bearbeiten")
         for mxd in self.task_config["mxd"]:
             self.logger.info(mxd["name"] + " wird bearbeitet.")
-            arcpy.mapping.MapDocument(mxd["ziel_akt"])
+            mxd_mapping = arcpy.mapping.MapDocument(mxd["ziel_akt"])
+            lyrfiles = arcpy.mapping.ListLayers(mxd_mapping)
+            for mxd_lyr in lyrfiles:
+                self.__replace(self, mxd_lyr, self.sde_conn_vek1_geo, self.sde_conn_ras1_geo_geo)
+                
             #TODO: umhängen für jedes lyr (funktion)
             arcpy.mapping.MapDocument(mxd["ziel_zs"])
-        
+            lyrfiles = arcpy.mapping.ListLayers(mxd_mapping)
+            for mxd_lyr in lyrfiles:
+                self.__replace(self, mxd_lyr, self.sde_conn_vek3_geo, self.sde_conn_ras1_geo_geo)
+            mxd_mapping.save()
        
         self.finish()
