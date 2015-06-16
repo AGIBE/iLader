93eb806b516090fcdd90b403d60c1d5dadb70165
Peter Schär <peter.schaer@bve.be.ch>
2015-06-16 16:09:05 +0200

    Funktion GeoDBProzess implementiert.

--- iLader/functions/GeoDBProzess.py
+++ iLader/functions/GeoDBProzess.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 from .TemplateFunction import TemplateFunction
+import cx_Oracle
+from arcpy.arcobjects.arcobjects import Schema
 
 class GeoDBProzess(TemplateFunction):
     '''
@@ -26,5 +28,30 @@ class GeoDBProzess(TemplateFunction):
         '''
         Führt den eigentlichen Funktionsablauf aus
         '''
+        db = self.task_config['instances']['work']
+        schema = self.task_config['schema']['gdbp']
+        username = 'gdbp'
+        password = self.task_config['users'][username]
+        
+        task_id = self.task_config['task_id']
+        gpr_code = self.task_config['gpr']
+        
+        sql = "UPDATE " + schema + ".GEOPRODUKTE SET TASK_ID=" + unicode(task_id) + " WHERE CODE='" + gpr_code + "'"
+        self.logger.info("SQL-Update wird ausgeführt: " + sql)
+        
+        connection = cx_Oracle.connect(username, password, db)  
+        cursor = connection.cursor()
+        cursor.execute(sql)
+ 
+        if cursor.rowcount == 1:
+            # Nur wenn genau 1 Zeile aktualisiert wurde, ist alles i.O. 
+            self.logger.info("Query wurde ausgeführt!")
+        else:
+            # Wenn nicht genau 1 Zeile aktualisiert wurde, wird Warnung ausgegeben
+            self.logger.warn("Query hat keinen oder mehr als einen Record aktualisiert!")
+ 
+        connection.commit()
+        del cursor
+        del connection
         
         self.finish()
