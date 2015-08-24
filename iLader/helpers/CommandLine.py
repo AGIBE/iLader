7fa6c3c0b74fba19c1df7e1399e8c470fb745d60
Peter Schär <peter.schaer@bve.be.ch>
2015-08-24 13:20:14 +0200

    Kommandozeile umgebaut und erweitert (Parameter load_taskconfig).

--- iLader/helpers/CommandLine.py
+++ iLader/helpers/CommandLine.py
@@ -5,31 +5,48 @@ from iLader import __version__
 import iLader.helpers.Helpers
 from iLader.usecases.Usecase import Usecase
 
+def run_taskid(args):
+    uc = Usecase(args.TASKID, args.load_taskconfig)
+    uc.run()
+
+def list_taskids(args):
+    tasks = iLader.helpers.Helpers.get_import_tasks()
+    for task in tasks:
+        print(task)
+        
+def encrypt(args):
+    #TODO: Befehl für encrypt_pw einfügen
+    print("Funktion ist im Moment noch nicht implementiert!")
+
+def decrypt(args):
+    #TODO: Befehl für decrypt_pw einfügen
+    print("Funktion ist im Moment noch nicht implementiert!")
+
 def main():
-    parser = argparse.ArgumentParser(description="Kommandozeile fuer den iLader. Fuehrt Tasks aus und zeigt offene Tasks an.", prog="iLader.exe")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-s", "--show_tasks", help="zeigt alle ausfuehrbaren Tasks an", action="store_true")
-    group.add_argument("-r", "--run_task", type=int, help="fuehrt den angegebenen Task aus", metavar="TASK-ID")
-    #TODO: Die Option für load_task_config einbauen. Wird nur bei run_task verwendet. Default ist False
-    group.add_argument("-v", "--version", help="zeigt die Programmversion an", action="store_true")
-    #TODO: Argument für encrypt_pw einfügen
-    #TODO: Argument für decrypt_pw einfügen
-    args = parser.parse_args()
+    version_text = "iLader v" + __version__
+    parser = argparse.ArgumentParser(description="Kommandozeile fuer den iLader. Fuehrt Tasks aus und zeigt offene Tasks an.", prog="iLader.exe", version=version_text)
+    subparsers = parser.add_subparsers(help='Folgende Befehle sind verfuegbar:')
     
-    if args.version:
-        print("iLader v" + __version__)
-    elif args.show_tasks:
-        tasks = iLader.helpers.Helpers.get_import_tasks()
-        for task in tasks:
-            print(task)
-    elif args.run_task:
-        #Abmachung: alle Zeichen vor dem ersten Doppelpunkt entsprechen der Task-ID
-        task_id = args.run_task
-        load_task_config = False
-        uc = Usecase(task_id, load_task_config)
-        uc.run()
-    else:
-        parser.print_help()
+    # LIST-Befehl
+    list_parser = subparsers.add_parser('list', help='zeigt alle ausfuehrbaren Tasks an.')
+    list_parser.set_defaults(func=list_taskids)
+    
+    # RUN-Befehl
+    run_parser = subparsers.add_parser('run', help='fuehrt den angegebenen Task aus.')
+    run_parser.add_argument("TASKID", type=int, help="auszufuehrende Task-ID.")
+    run_parser.add_argument("-l", "--load_taskconfig", help="liest die bestehende Task-Config ein.", action="store_true", required=False)
+    run_parser.set_defaults(func=run_taskid)
+    
+    # ENCRYPT-Befehl
+    encrypt_parser = subparsers.add_parser('encrypt', help="verschluesselt das eingegebene Passwort (Nicht implementiert!).")
+    encrypt_parser.set_defaults(func=encrypt)
+    
+    # DECRYPT-Befehl
+    decrypt_parser = subparsers.add_parser('decrypt', help="entschluesselt das eingegebene Passwort (Nicht implementiert!).")
+    decrypt_parser.set_defaults(func=decrypt)
+    
+    args = parser.parse_args()
+    args.func(args)
     
 if __name__ == "__main__":
     main()
