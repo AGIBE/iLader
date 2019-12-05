99e17df14833b7789debe3e611d0b44ce507f8d1
Peter Schaer <peter.schaer@bve.be.ch>
2019-12-05 15:34:25 +0100

    #3533 keine SHAPE-Felder mehr bei Standalone-Tabellen in PostGIS

--- iLader/functions/KopieVek1Ersatz_PG.py
+++ iLader/functions/KopieVek1Ersatz_PG.py
@@ -6,6 +6,7 @@ import os
 from iLader.helpers import PostgresHelper
 from iLader.helpers import FME_helper
 
+
 class KopieVek1Ersatz_PG(TemplateFunction):
     '''
     Kopiert saemtliche Vektorebenen aus der Instanz TEAM in die Instanz vek1 (PostgreSQL). Folgende Typen
@@ -23,6 +24,7 @@ class KopieVek1Ersatz_PG(TemplateFunction):
     
     Der raeumlichen Indexes kann ebenfalls aufgrund der Locks nicht neu berechnet werden.
     '''
+
     def __init__(self, task_config):
         '''
         Constructor
@@ -30,15 +32,16 @@ class KopieVek1Ersatz_PG(TemplateFunction):
         '''
         self.name = "KopieVek1Ersatz_PG"
         TemplateFunction.__init__(self, task_config)
-        
-        if self.name in self.task_config['ausgefuehrte_funktionen'] and self.task_config['task_config_load_from_JSON']:
+
+        if self.name in self.task_config['ausgefuehrte_funktionen'
+                                         ] and self.task_config[
+                                                 'task_config_load_from_JSON']:
             self.logger.info("Funktion " + self.name + " wird ausgelassen.")
         else:
             self.logger.info("Funktion " + self.name + " wird ausgefuehrt.")
             self.start()
             self.__execute()
-            
-        
+
     def __execute(self):
         '''
         Fuehrt den eigentlichen Funktionsablauf aus
@@ -50,86 +53,113 @@ class KopieVek1Ersatz_PG(TemplateFunction):
         schema = self.task_config['schema']['geodb']
         pw = self.task_config['users']['geodb']
         source_sde = self.task_config['connections']['sde_conn_norm']
-        fme_script = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')) + "\\helpers\\" + "EsriGeodatabase2PostGIS.fmw"
-        
+        fme_script = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), '..')
+        ) + "\\helpers\\" + "EsriGeodatabase2PostGIS.fmw"
+
         # Jede Ebene durchgehen
         for ebene in self.task_config['vektor_ebenen']:
-            source = ebene['quelle']       
-            source_table = source.rsplit('\\',1)[1]
-            table = ebene['ziel_vek1'].lower().rsplit('\\',1)[1]
+            source = ebene['quelle']
+            source_table = source.rsplit('\\', 1)[1]
+            table = ebene['ziel_vek1'].lower().rsplit('\\', 1)[1]
             ebename = ebene['gpr_ebe'].lower()
-            
+
             self.logger.info("Ebene " + ebename + " wird nach vek1 kopiert.")
             self.logger.info("Quelle: " + source)
-            self.logger.info("Ziel: " + host + "/" + db +" "+ table)
-            
+            self.logger.info("Ziel: " + host + "/" + db + " " + table)
+
+            # Prüfen, ob es eine Featureclass oder eine Standalone-Tabelle ist
+            tabellentyp = "featureclass"
+            if ebene['datentyp'] in ('Tabelle', 'unbekannt', 'Wertetabelle'):
+                tabellentyp = "table"
+
             # Pruefen ob es die source Tabelle gibt
             if not arcpy.Exists(source):
                 # Existiert die Quell-Ebene nicht, Abbruch mit Fehlermeldung und Exception
                 self.logger.error("Quell-Ebene " + source + " existiert nicht!")
                 raise Exception
-        
+
             # Pruefen ob es die target Tabelle gibt
             table_sp = table.split('.')
-            sql_query = "SELECT 1 FROM information_schema.tables WHERE table_schema = '" + table_sp[0] + "' AND table_name = '" + table_sp[1] + "'"
-            result_vek1 = PostgresHelper.db_sql(self, host, db, db_user, port, pw, sql_query, True)
+            sql_query = "SELECT 1 FROM information_schema.tables WHERE table_schema = '" + table_sp[
+                    0] + "' AND table_name = '" + table_sp[1] + "'"
+            result_vek1 = PostgresHelper.db_sql(
+                    self, host, db, db_user, port, pw, sql_query, True
+            )
             if result_vek1 is None:
                 # Gibt es die Ziel-Ebene noch nicht, Abbruch mit Fehlermeldung und Exception
                 self.logger.error("Ziel-Ebene " + table + " existiert nicht!")
                 raise Exception
-            
+
             # Eine Liste der Attribute mit Typ Date erstellen (FME braucht diese Info, damit leere Datumsfelder fuer Postgres richtig gesetzt werden koennen)
-            datefields = arcpy.ListFields(source,field_type='Date')
+            datefields = arcpy.ListFields(source, field_type='Date')
             dfield = None
             for datefield in datefields:
                 if dfield is None:
                     dfield = datefield.name
                 else:
                     dfield = dfield + ' ' + datefield.name
-            
+
             # Anzahl Records in Source
             count_source = int(arcpy.GetCount_management(source)[0])
-            
+
             # Daten kopieren
             # Copy-Script, Table Handling auf Truncate umstellen, damit Tabelle nicht gelöscht wird
-            self.logger.info("Ebene " + host + "/" + db +" "+ table + " wird geleert (Truncate) und aufgefuellt (Insert).")
+            self.logger.info(
+                    "Ebene " + host + "/" + db + " " + table +
+                    " wird geleert (Truncate) und aufgefuellt (Insert)."
+            )
             # Extra truncate bei 0 Quell-Records, da FME bei 0 Quelldaten kein Truncate macht.
             if count_source == 0:
                 sql_query = 'TRUNCATE ' + table
-                PostgresHelper.db_sql(self, host, db, db_user, port, pw, sql_query)
-            
-            fme_logfile = FME_helper.prepare_fme_log(fme_script, (self.task_config['log_file']).rsplit('\\',1)[0])
+                PostgresHelper.db_sql(
+                        self, host, db, db_user, port, pw, sql_query
+                )
+
+            fme_logfile = FME_helper.prepare_fme_log(
+                    fme_script, (self.task_config['log_file']).rsplit('\\',
+                                                                      1)[0]
+            )
             # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
             # Daher muessen workspace und parameters umgewandelt werden!
             parameters = {
-                'TABELLEN': str(source_table),
-                'POSTGIS_DB': str(db),
-                'POSTGIS_HOST': str(host),
-                'POSTGIS_PORT': str(port),
-                'POSTGIS_USER': str(db_user),
-                'SCHEMA_NAME': str(schema),
-                'POSTGIS_PASSWORD': str(pw),
-                'LOGFILE': str(fme_logfile),
-                'INPUT_SDE': str(source_sde),
-                'TABLE_HANDLING': "TRUNCATE_EXISTING",
-                'DATEFIELDS': str(dfield)
+                    'TABELLEN': str(source_table),
+                    'POSTGIS_DB': str(db),
+                    'POSTGIS_HOST': str(host),
+                    'POSTGIS_PORT': str(port),
+                    'POSTGIS_USER': str(db_user),
+                    'SCHEMA_NAME': str(schema),
+                    'POSTGIS_PASSWORD': str(pw),
+                    'LOGFILE': str(fme_logfile),
+                    'INPUT_SDE': str(source_sde),
+                    'TABLE_HANDLING': "TRUNCATE_EXISTING",
+                    'DATEFIELDS': str(dfield),
+                    'TABELLENTYP': str(tabellentyp)
             }
             # FME-Skript starten
             FME_helper.fme_runner(self, str(fme_script), parameters)
-            
+
             # Check ob in Quelle und Ziel die gleiche Anzahl Records vorhanden sind
-            self.logger.info("Anzahl Objekte in Quell-Ebene: " + unicode(count_source))
+            self.logger.info(
+                    "Anzahl Objekte in Quell-Ebene: " + unicode(count_source)
+            )
             sql_query = 'SELECT COUNT(*) FROM ' + table
-            count_target = PostgresHelper.db_sql(self, host, db, db_user, port, pw, sql_query, True)
-            self.logger.info("Anzahl Objekte in Ziel-Ebene: " + unicode(count_target))
-               
+            count_target = PostgresHelper.db_sql(
+                    self, host, db, db_user, port, pw, sql_query, True
+            )
+            self.logger.info(
+                    "Anzahl Objekte in Ziel-Ebene: " + unicode(count_target)
+            )
+
             if count_source != int(count_target):
-                self.logger.error("Anzahl Objekte in Quelle und Ziel unterschiedlich!")
+                self.logger.error(
+                        "Anzahl Objekte in Quelle und Ziel unterschiedlich!"
+                )
                 raise Exception
             else:
                 self.logger.info("Anzahl Objekte in Quelle und Ziel identisch!")
-            
+
             self.logger.info("Ebene " + ebename + " wurde ersetzt")
-        
-        self.logger.info("Alle Ebenen wurden ersetzt.")       
+
+        self.logger.info("Alle Ebenen wurden ersetzt.")
         self.finish()
