63462c6e90d22fa3918249bd67438958e48318ad
Peter Schaer <peter.schaer@be.ch>
2020-09-23 09:47:01 +0200

    #6992 nicht mehr relevanten Test entfernt

--- src/iLader/tests/run_usecase_neues_geoproduct.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-from iLader.usecases import Usecase
-
-uc = Usecase("200159", False)
-uc.run()
-print(uc.name)
-print(uc.task_id)
-print(uc.general_config)
